9a11676b184989bde446fa5e88cbbcb3ae794a87
wmancaniello <wmancaniello@gmail.com>
2024-04-11 18:17:51 +0200

    add pint to HTML

--- js/script.js
+++ js/script.js
@@ -4,39 +4,60 @@ const memberTeam = [
   {
     name: "Wayne Barnett",
     role: "Founder & CEO",
-    photo: "wayne-barnett-founder-ceo.jpg",
+    photo: "./img/wayne-barnett-founder-ceo.jpg",
   },
   {
     name: "Angela Caroll",
     role: "Chief Editor",
-    photo: "angela-caroll-chief-editor.jpg",
+    photo: "./img/angela-caroll-chief-editor.jpg",
   },
   {
     name: "Walter Gordon",
     role: "Office Manager",
-    photo: "walter-gordon-office-manager.jpg",
+    photo: "./img/walter-gordon-office-manager.jpg",
   },
   {
     name: "Angela Lopez",
     role: "Social Media Manager",
-    photo: "angela-lopez-social-media-manager.jpg",
+    photo: "./img/angela-lopez-social-media-manager.jpg",
   },
   {
     name: "Scott Estrada",
     role: "Developer",
-    photo: "scott-estrada-developer.jpg",
+    photo: "./img/scott-estrada-developer.jpg",
   },
   {
     name: "Barbara Ramos",
     role: "Graphic Designer",
-    photo: "barbara-ramos-graphic-designer.jpg",
+    photo: "./img/barbara-ramos-graphic-designer.jpg",
   },
 ];
 
-
-
 //  Stampo in console le informazioni di ciascun membro del team
 // utilizzo forEach()
-memberTeam.forEach(member => {
-    console.log(`Name: ${member.name}, Role: ${member.role}, Photo:${member.photo}`);
+memberTeam.forEach((member) => {
+  console.log(
+    `Name: ${member.name}, Role: ${member.role}, Photo:${member.photo}`
+  );
+});
+
+// Prelevo il div per inserire  le card in pagina
+const teamCard = document.getElementById("team-card");
+
+memberTeam.forEach((member) => {
+  // Creo le stringhe da stampare
+  const printToHtml = `
+        <div class="col-lg-4 col-md-6 mb-2">
+            <div class="card">
+                <img src="${member.photo}" alt="">
+                <div class="card-body">
+                    <h5 class="card-title text-center">${member.name}</h5>
+                    <p class="card-text text-center">${member.role}</p>
+                </div>
+            </div>
+        </div>
+    `;
+
+  // Stampa in pagina
+  teamCard.innerHTML += printToHtml;
 });
